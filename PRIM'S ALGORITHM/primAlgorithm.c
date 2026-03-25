1556701a9316ee043206f36e4e45db4f0704c746
Rahul Rajput <rahulrajput.cs24@bmsce.ac.in>
2026-03-25 15:30:46 +0530

    Add files via upload

---
+++ PRIM'S ALGORITHM/primAlgorithm.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 100
+
+int n;
+
+int minKey(int key[], int mstSet[]) {
+    int min = INT_MAX, min_index = -1;
+
+    for (int v = 0; v < n; v++) {
+        if (mstSet[v] == 0 && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+void printMST(int parent[], int graph[MAX][MAX]) {
+    printf("\nEdge \tWeight\n");
+    for (int i = 1; i < n; i++) {
+        printf("%d - %d \t%d\n", parent[i], i, graph[i][parent[i]]);
+    }
+}
+
+void primMST(int graph[MAX][MAX]) {
+    int parent[MAX];
+    int key[MAX];
+    int mstSet[MAX];
+
+    for (int i = 0; i < n; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = 0;
+    }
+
+    key[0] = 0;
+    parent[0] = -1;
+
+    for (int count = 0; count < n - 1; count++) {
+        int u = minKey(key, mstSet);
+        mstSet[u] = 1;
+
+        for (int v = 0; v < n; v++) {
+            if (graph[u][v] != 0 && mstSet[v] == 0 && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    printMST(parent, graph);
+}
+int main() {
+    int graph[MAX][MAX];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    primMST(graph);
+
+    return 0;
+}
