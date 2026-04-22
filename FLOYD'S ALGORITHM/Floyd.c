288be5a798af5aa37914abfd9d41f0ea0b71758f
Rahul Rajput <rahulrajput.cs24@bmsce.ac.in>
2026-04-22 15:32:00 +0530

    Add files via upload

---
+++ FLOYD'S ALGORITHM/Floyd.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define INF 99999
+
+int main() {
+    int V;
+    printf("Enter number of vertices: ");
+    scanf("%d", &V);
+    int graph[V][V];
+    printf("Enter the adjacency matrix:\n");
+    printf("(Enter %d for INF if no edge exists)\n", INF);
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    int dist[V][V];
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    printf("\nShortest distance matrix:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
